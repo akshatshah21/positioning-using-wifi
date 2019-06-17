710e6d2009320629e4fbbd82c7a01c183d6d35c6
akshatshah21 <akshatshah_21@yahoo.in>
2019-06-17 11:21:53 +0530

    Added to complexity1 src

--- complexity1/src/complexity1_laptop.py
+++ complexity1/src/complexity1_laptop.py
@@ -8,12 +8,17 @@ mqttPwd = 'u7RF9Xts1g1r'
 mqttPort = 16557
 mqttClient = mqtt.Client('Laptop',True)
 
+file_list = [open("file_{}".format(x),'w') for x in range(10)]
 
 ssid_list = []
-
+file_dict = {} #Empty dictionary for ssid-file object links
+ssid_count = 0
 def on_message(client, userdata, message):
+    global ssid_list,ssid_count,file_dict
     if str(message.topic) == 'ssid':
+        ssid_count += 1
         ssid_list.append(str(message.payload.decode()))
+        file_dict[ssid_count - 1] = file_list[ssid_count - 1]
     if str(message.topic) == 'a1':
         msg = message.payload.decode()
         print(msg)
@@ -21,7 +26,10 @@ def on_message(client, userdata, message):
         ssid = str(separated_msg[0])
         for temp in ssid_list:
             if ssid == temp:
+                f = file_list[temp.index()]
+                f.write(separated_msg[1] + '\n')
                 #Use dictionary here. temp must be key. Return value must be file object
+
 
 
 
