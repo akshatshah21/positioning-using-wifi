338e50559ee4895eb4eff612cc5a00e1619c59c2
akshatshah21 <akshatshah_21@yahoo.in>
2019-06-17 11:41:09 +0530

    Added to complexity1 src

--- complexity1/src/complexity1_laptop.py
+++ complexity1/src/complexity1_laptop.py
@@ -8,28 +8,40 @@ mqttPwd = 'u7RF9Xts1g1r'
 mqttPort = 16557
 mqttClient = mqtt.Client('Laptop',True)
 
-file_list = [open("file_{}".format(x),'w') for x in range(10)]
+file_list_a1 = [open("a1/file_{}".format(x),'w') for x in range(10)]
+file_list_a2 = [open("a2/file_{}".format(x),'w') for x in range(10)]
+file_list_a3 = [open("a3/file_{}".format(x),'w') for x in range(10)]
 
 ssid_list = []
 file_dict = {} #Empty dictionary for ssid-file object links
 ssid_count = 0
 def on_message(client, userdata, message):
     global ssid_list,ssid_count,file_dict
-    if str(message.topic) == 'ssid':
+    if str(message.topic) == 'ssid':    #Messages will have ssid
         ssid_count += 1
-        ssid_list.append(str(message.payload.decode()))
-        file_dict[ssid_count - 1] = file_list[ssid_count - 1]
-    if str(message.topic) == 'a1':
+        ssid_received = str(message.payload.decode())
+        if ssid_received not in ssid_list:
+            ssid_list.append(ssid_received)
+            file_dict[ssid_received] = file_list[ssid_count - 1]
+    if str(message.topic) == 'a1':      #Message in the form of ssid:distance
         msg = message.payload.decode()
         print(msg)
         separated_msg = msg.split(':')
         ssid = str(separated_msg[0])
         for temp in ssid_list:
             if ssid == temp:
-                f = file_list[temp.index()]
+                f = file_list_a1[temp.index()]
                 f.write(separated_msg[1] + '\n')
                 #Use dictionary here. temp must be key. Return value must be file object
-
+    if str(message.topic) == 'a2':
+        msg = message.payload.decode()
+        print(msg)
+        separated_msg = msg.split(':')
+        ssid = str(separated_msg[0])
+        for temp in ssid_list:
+            if ssid == temp:
+                f = file_list_a2[temp.index()]
+                f.write(separated_msg[1] + '\n')
 
 
 
